956dbc7d690c4784a5b704998680e97d33e103d7
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2020-12-14 13:59:20 -0300

    testing a class - fixing bugs

---
+++ tests/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testReturnFullName()
+    {
+        require 'User.php';
+
+        $user = new User;
+
+        $user->first_name = "Teresa";
+        $user->surname = "Green";
+
+        $this->assertEquals('Teresa Green', $user->getFullName());
+    }
+
+    public function testFullNameIsEmptyByDefault()
+    {
+        $user = new User;
+
+        $this->assertEquals('', $user->getFullName());
+    }
+}
