85ac282632a52925c57a3494b591f0abb9bf921b
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2020-12-15 14:31:10 -0300

    Using Mockery to mock a dependency that doesn't exist yet

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -4,17 +4,39 @@ use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
 {
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     public function testOrderIsProcessed()
     {
         $gateway = $this->getMockBuilder('PaymentGateway')
-                        ->setMethods(['charge'])
-                        ->getMock();
+            ->setMethods(['charge'])
+            ->getMock();
 
         $gateway->expects($this->once())
-                ->method('charge')
-                ->with($this->equalTo(200))
-                ->willReturn(true);
-        
+            ->method('charge')
+            ->with($this->equalTo(200))
+            ->willReturn(true);
+
+        $order = new Order($gateway);
+
+        $order->amount = 200;
+
+        $this->assertTrue($order->process());
+    }
+
+    public function testOrderIsProcessedUsingMockery()
+    {
+        $gateway = Mockery::mock('PaymentGateway');
+
+        $gateway->shouldReceive('charge')
+            ->once()
+            ->with(200)
+            ->andReturn(true);
+
         $order = new Order($gateway);
 
         $order->amount = 200;
