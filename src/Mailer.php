c74c19a80f7b1269c6069e5812714e7d0509d608
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2020-12-16 09:24:23 -0300

    Testing static methods

--- src/Mailer.php
+++ src/Mailer.php
@@ -28,4 +28,25 @@ class Mailer
 
         return true;
     }
+
+    /**
+     * Send a message
+     *
+     * @param string $email  Recipient email address
+     * @param string $message  Content of the message
+     *
+     * @throws InvalidArgumentException If $email is empty
+     *
+     * @return boolean
+     */
+    public static function send(string $email, string $message)
+    {
+        if (empty($email)) {
+            throw new InvalidArgumentException;
+        }
+
+        echo "Send '$message' to $email";
+
+        return true;
+    }
 }
