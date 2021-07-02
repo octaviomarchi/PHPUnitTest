06709ab7d2d7d174bf93a068518b48d9f23fa5bf
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2021-07-02 10:00:12 -0300

    phpcs file test fixed for psr12

--- src/PhpCodeSnifferTest.php
+++ src/PhpCodeSnifferTest.php
@@ -1,13 +1,37 @@
 <?php
 
-
+/**
+ * File doc
+ * PHP version 7.4
+ * 
+ * @category Category
+ * @package  Package
+ * @author   Octavio <octavio.marchi@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link     5
+ */
+/**
+ * Class doc
+ * 
+ * @category Category
+ * @package  Package
+ * @author   Octavio <octavio.marchi@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link     5
+ */
 class PhpCodeSnifferTest
 {
 
     public $string = "Some text here";
 
-    function test () {
-        if($string  ){  
+    /**
+     *  Func doc
+     * 
+     * @return void
+     */
+    function test() 
+    {
+        if ($string) {  
             print_r($string);
         }
     }
