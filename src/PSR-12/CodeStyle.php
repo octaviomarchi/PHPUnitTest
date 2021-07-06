5c5437c0043bb960d8392a7ce0bf9444f7e73d29
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2021-07-06 16:32:50 -0300

    wrong code styles

---
+++ src/PSR-12/CodeStyle.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * File doc
+ * PHP version 7.4
+ *
+ * @category Category
+ * @package  Package
+ * @author   Octavio <octavio.marchi@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link     5
+ */
+
+/**
+ * Class doc
+ *
+ * @category Category
+ * @package  Package
+ * @author   Octavio <octavio.marchi@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link     5
+ */
+class CodeStyle
+{
+    var $Foo = "bar";
+
+    /**
+     * small description
+     *
+     * @param $bar bar
+     *
+     * @return void
+     */
+    function foo($bar)
+    {
+        if(  !empty($bar)){
+            echo $bar;
+        }
+    }
+}
